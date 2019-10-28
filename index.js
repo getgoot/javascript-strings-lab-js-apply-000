ee4d9796488831e92c97d713f8a5cd3a1d086764
epic-method-7082 <elliotg283@gmail.com>
2019-10-28 11:21:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-"Hello, everybody!"
+Function; var greeting = "Hello, everybody!";
+var specialGuest = "Neil deGrasse Tyson";
+var greetSpecialGuest = "" +  specialGuest + "!";
+var topic = "space";
+var conversation = `${topic}`;
+var greeting = "Hello, everybody!";
